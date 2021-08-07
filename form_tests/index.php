0453ceb8c7a617caffa8e59385ec2ebe2a658bf9
Jessica Kincaid <4j.kincaid@gmail.com>
2021-08-07 12:07:59 -0500

    sample from PHP lesson

---
+++ form_tests/index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>GET Superglobal</title>
+</head>
+
+<body>
+
+<?php 
+if (isset($_GET['name'], $_GET['lang'])){
+    $name = $_GET['name'];
+    $lang = $_GET['lang'];
+    echo $name . '<br>';
+    echo $lang;
+}
+
+?>
+
+<form method="get" action="index.php">
+  <input type="text" name="name">
+  <input type="text" name="lang">
+  <input type="submit" value="submit">
+</form>
+
+</body>
+
+</html>
